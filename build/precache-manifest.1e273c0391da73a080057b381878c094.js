d9c6ef8a50d05f1a3b8aa8ca77c46142c7a21622
mdbassoon <mark.delgado87@gmail.com>
2021-06-14 12:33:16 -0700

    changed homepage config

---
+++ build/precache-manifest.1e273c0391da73a080057b381878c094.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38f5a4dfd1d979cbd12922559d37734e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2835e008ec53de41376",
+    "url": "/static/css/main.de5d2a55.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "e2835e008ec53de41376",
+    "url": "/static/js/main.b54fb2b4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
