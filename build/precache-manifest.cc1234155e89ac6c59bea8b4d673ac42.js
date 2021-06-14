8fe36f30a2be2d0ea8f4ede74ab21b1a3ac7ebef
mdbassoon <mark.delgado87@gmail.com>
2021-06-14 12:14:52 -0700

    added build to github

---
+++ build/precache-manifest.cc1234155e89ac6c59bea8b4d673ac42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3069e6fb978d160462f299e69327c87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "252f9f5a68b3b60541d8",
+    "url": "/static/css/main.de5d2a55.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "252f9f5a68b3b60541d8",
+    "url": "/static/js/main.5559d397.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
